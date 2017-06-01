80cd84c372aee649b798c30ea3c211aa9fb10ca8
yonatan <ykyonatan@gmail.com>
2017-06-01 16:48:24 +0200

    added login page for the link

---
+++ js/login.js
@@ -0,0 +1,8 @@
+var password;
+var pass = "test20";
+password = prompt("Please enter password to continue");
+if(password === pass){
+  alert("correct, press ok to continue");
+}else{
+  alert("wrong, try again");
+}
